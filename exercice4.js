93d38d4710ab9ee54f00acd659b1c612184e3d7d
JonathanLebourg <jonjonlebourg@gmail.com>
2018-10-08 15:46:04 +0200

    fini

---
+++ exercice4.js
@@ -0,0 +1,15 @@
+var lastName= prompt("tapez votre nom");
+var firstName= prompt("Bonjour, tapez votre prénom");
+var city= prompt("Bonjour, tapez votre ville");
+
+alert("Nom: "+lastName+"\nPrénom: "+firstName+"\nVille: "+city);
+
+
+
+
+//
+// var lastName= document.getElementById('lastName');
+// var firstName= document.getElementById('firstName');
+// var city= document.getElementById('city')
+//
+// alert("Nom: "+lastName+"\nPrénom: "+firstName+"\nVille: "+city);
