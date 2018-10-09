11c6927488b96bc60bf9c417036eda86532a726d
JonathanLebourg <jonjonlebourg@gmail.com>
2018-10-09 14:20:00 +0200

    mis en page

--- exercice4Id.js
+++ exercice4Id.js
@@ -1,8 +1,21 @@
+
 function validation(){
 
-var lastName= document.getElementById('lastname').value;
-var firstName= document.getElementById('firstname').value;
-var city= document.getElementById('city').value;
+//definition des variables
+  var lastName= document.getElementById('lastname').value;
+  var firstName= document.getElementById('firstname').value;
+  var city= document.getElementById('city').value;
+  var regex= /^[a-zA-Zéèëï]+$/;
+  var text= firstName+lastName+city; //concatenation
 
-alert("Nom: "+lastName+"\nPrénom: "+firstName+"\nVille: "+city);
+//enchainement des regex
+if (!regex.test(text)||lastName==null){
+    alert("Problème de saisie");}
+else
+  if (firstName==null){alert("Problème de saisie")}
+    else
+      if (city==null){alert("Problème de saisie")}
+//sinon bonne réponse
+  else{alert("Nom: "+lastName+"\nPrénom: "+firstName+"\nVille: "+city);}
+
 }
